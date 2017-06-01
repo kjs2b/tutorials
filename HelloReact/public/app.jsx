573f462fe87a00b1a49b7da4fdea9da365d15a75
Kevin <kjs2b@virginia.edu>
2017-06-01 13:38:06 -0700

    Added form to page for submission of user name

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,14 +1,36 @@
 var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is from the component.'
+    };
+  },
+
+  onButtonClick(e) {
+    e.preventDefault();
+    var name = this.refs.name.value;
+    alert(name);
+  },
+
   render: function () {
     return (
       <div>
-        <h1>Hello React!</h1>
+        <h1>Hello {this.props.name}</h1>
+        <p>{this.props.message + '!!'}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type='text' ref='name' />
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
 });
 
+var firstName = 'Kevin';
+var message = 'Take me to your leader';
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message={message}/>,
   document.getElementById('app')
 );
